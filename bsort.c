9ef27c819e122dd40a09316d1ad3ccc9079ad07f
vAlAi <valai@valaiMac.local>
2017-07-21 17:40:45 +0900

    bsortの速度向上

--- bsort.c
+++ bsort.c
@@ -12,16 +12,20 @@ void bsort(char *string_array, int size) {
 	char *s = (char*)malloc(sizeof(char)*size);
 	int i, j;
 
-	int **order = (int**)malloc(sizeof(int*)*size);
+	int *order = (int*)malloc(sizeof(int)*size);
+	/*
 	for(i = 0; i < size; i++){
-		order[i] = (int*)malloc(sizeof(int)*2);
+		order[i] = (int*)malloc(sizeof(int));
 	}
-
+	*/
+	start = clock();
 	i = 0;
 	for(i = 0; i < size; i++){
-		order[i][0] = i;
-		order[i][1] = size + i - 1;
+		order[i] = i;
+		//order[i][1] = size + i - 1;
 	}
+	end = clock();
+	printf("%fsec\n", (double)(end - start)/CLOCKS_PER_SEC);
 
 	strcpy(w, string_array);
 	strcat(w, string_array);
@@ -29,41 +33,45 @@ void bsort(char *string_array, int size) {
 	mysort(w, order, size);
 
 	for(j = 0; j < size; j++){
-		s[j] = w[order[j][1]];
+		s[j] = w[order[j] + size - 1];
 	}
 	s[j] = '\0';
 
 	strcpy(string_array, s);
-
+/*
 	for(i = 0; i < size; i++){
 		free(order[i]);
 	}
+	*/
 	free(order);
 	free(w);
 	free(s);
 
 }
 
-int mysort(char *string_array, int **order, int size) {
+int mysort(char *string_array, int *order, int size) {
   int ret;
   //input your code here...
 	int i;
-	int **t_order = (int**)malloc(sizeof(int*)*size);
+	int *t_order = (int*)malloc(sizeof(int)*size);
+	/*
 	for(i = 0; i < size; i++){
-		t_order[i] = (int*)malloc(sizeof(int)*2);
+		t_order[i] = (int*)malloc(sizeof(int));
 	}
+	*/
 
 	Msort(string_array, order, t_order, 0, size - 1, size);
-
+/*
 	for(i = 0; i < size; i++)
 		free(t_order[i]);
+		*/
 	free(t_order);
 
   ret = definecheck(918210);
   return ret;
 }
 
-void Msort(char *Data, int **order, int **t_order, int left, int right, int size){
+void Msort(char *Data, int *order, int *t_order, int left, int right, int size){
 	int mid, i, j, k;
 
 	if(left >= right){
@@ -75,8 +83,8 @@ void Msort(char *Data, int **order, int **t_order, int left, int right, int size
 	Msort(Data, order, t_order, mid + 1, right, size);
 
 	for(i = left; i <= right; i++){
-		t_order[i][0] = order[i][0];
-		t_order[i][1] = order[i][1];
+		t_order[i] = order[i];
+//		t_order[i][1] = order[i][1];
 	}
 	
 	i = left;
@@ -84,13 +92,13 @@ void Msort(char *Data, int **order, int **t_order, int left, int right, int size
 	k = left;
 
 	while(i <= mid || j <= right){
-		if(j > right || (i <= mid && strncmp(Data + t_order[i][0], Data + t_order[j][0], size) <= 0)){
-			order[k][0] = t_order[i][0];
-			order[k][1] = t_order[i][1];
+		if(j > right || (i <= mid && strncmp(Data + t_order[i], Data + t_order[j], size) <= 0)){
+			order[k] = t_order[i];
+//			order[k][1] = t_order[i][1];
 			i++;
 		}else{
-			order[k][0] = t_order[j][0];
-			order[k][1] = t_order[j][1];
+			order[k] = t_order[j];
+//			order[k][1] = t_order[j][1];
 			j++;
 		}
 		k++;
